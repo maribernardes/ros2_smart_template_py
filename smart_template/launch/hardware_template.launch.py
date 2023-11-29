f42db9675df2346ee732cb1c6481499972d4ce4a
maribernardes <maribernardes@gmail.com>
2023-11-29 13:36:46 -0500

    Separated launch for robot in virtual and hardware option

---
+++ smart_template/launch/hardware_template.launch.py
@@ -0,0 +1,31 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription
+
+import sys
+
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+from launch import LaunchDescription, actions
+from launch.actions import DeclareLaunchArgument
+
+# Launch stage control in mcp mode
+# Remember to launch bridge server in another terminal
+# Remember to run keypress node (trajcontrol package) in another terminal
+
+def generate_launch_description():
+
+    # Use smart template node
+    robot = Node(
+        package="smart_template",
+        executable="template",
+    )  
+
+    return LaunchDescription([
+        robot
+    ])
--- smart_template/launch/robot.launch.py
+++ smart_template/launch/robot.launch.py
@@ -1,31 +1,42 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
+
+from launch import LaunchDescription, conditions
+from launch.substitutions.launch_configuration import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.substitutions import PythonExpression
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import IncludeLaunchDescription
 
-import sys
-
-from ament_index_python.packages import get_package_share_directory
-from launch_ros.actions import Node
-from launch import LaunchDescription, actions
-from launch.actions import DeclareLaunchArgument
-
-# Launch stage control in mcp mode
-# Remember to launch bridge server in another terminal
-# Remember to run keypress node (trajcontrol package) in another terminal
+# Launch robot (hardware or virtual version)
 
 def generate_launch_description():
 
-    # Use smart template node
-    robot = Node(
-        package="smart_template",
-        executable="template",
+    ld = LaunchDescription()
+
+    arg_sim_level = DeclareLaunchArgument(
+        "sim_level",
+        default_value = "2",
+        description = "virtual = 1, real = 2"
     )  
 
-    return LaunchDescription([
-        robot
-    ])
+    launch_directory = os.path.join(get_package_share_directory('smart_template'), 'launch')
+    hardware_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(launch_directory, 'hardware_template.launch.py')),
+        condition=conditions.IfCondition(
+            PythonExpression([LaunchConfiguration('sim_level'), " == 2"])
+        )
+    )
+    virtual_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(launch_directory, 'virtual_template.launch.py')),
+        condition=conditions.IfCondition(
+            PythonExpression([LaunchConfiguration('sim_level'), " == 1"])
+        )
+    )
+
+    # Include launch arguments
+    ld.add_action(arg_sim_level)
+    
+    ld.add_action(hardware_launch)
+    ld.add_action(virtual_launch)
+    
+    return ld
